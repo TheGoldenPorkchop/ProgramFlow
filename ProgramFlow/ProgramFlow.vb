f0c7668b735734b1e844e3d3631c265e10ecfa87
TheGoldenPorkchop <angelnavamusic@gmail.com>
2025-01-27 08:42:46 -0700

    Added Else Statement

--- ProgramFlow/ProgramFlow.vb
+++ ProgramFlow/ProgramFlow.vb
@@ -32,6 +32,22 @@ Module ProgramFlow
             Console.WriteLine(chumpChange > 4)
         End If
 
+        'new piece of code
+        Console.WriteLine()
+
+        'assign -3 to chumpChange (chumpChange is now 2)
+        chumpChange -= 3
+
+        'if none of the conditions evaluates to true the Else case triggers
+        If chumpChange > 6 Then
+            Console.WriteLine($"{chumpChange} is greater than 6")
+        ElseIf chumpChange > 3 Then
+            Console.WriteLine($"{chumpChange} is greater than 3")
+        ElseIf chumpChange > 4 Then
+            Console.WriteLine($"{chumpChange} is greater than 4")
+        Else
+            Console.WriteLine("How did you end up here?")
+        End If
     End Sub
 
 End Module
